30dc215147f8a9e8b534002b19ecbcef7ce4f886
Abhik Baidya <abhikbaidya7@gmail.com>
2024-09-16 15:36:33 +0530

    app commit

---
+++ app/index.jsx
@@ -0,0 +1,19 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+const index = () => {
+  return (
+    <View className=" flex-1 bg-white items-center justify-center">
+      <Text className="text-3xl font-pblack">Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to profile
+      </Link>
+    </View>
+  );
+};
+
+export default index;
