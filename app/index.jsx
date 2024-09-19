ee7d6900ce01b35aa4caab15d41dc858aa4c3cfa
Abhik Baidya <abhikbaidya7@gmail.com>
2024-09-19 17:22:50 +0530

    app commit

--- app/index.jsx
+++ app/index.jsx
@@ -8,7 +8,7 @@ import { CustomButton } from "../components/CustomButton";
 import { Loader } from "../components/Loader";
 import { useGlobalContext } from "../context/GlobalProvider";
 
-const Welcome = () => {
+export function Welcome() {
   const { isLoading, isLoggedIn } = useGlobalContext();
   if (!isLoading && isLoggedIn) return <Redirect href="/home" />;
   return (
@@ -62,6 +62,4 @@ const Welcome = () => {
       <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
-};
-
-export default Welcome;
+}
