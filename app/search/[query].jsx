dfb2bd388ed78a600e0e79d3e3dd898c6a0b0d14
Abhik Baidya <abhikbaidya7@gmail.com>
2024-09-26 18:42:25 +0530

    app commit

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,11 +1,8 @@
 import { View, Text, FlatList } from "react-native";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
-import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
-import { getAllPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
 import { searchPosts } from "../../lib/appwrite";
